01f0ec978c69b85f0a32ff1fea5e5bcb536b8af4
Xin-Xin Wang <xin-xinw@musiclifephilosophy.com>
2015-06-27 21:59:26 -0400

    add contfracaneq2ns1.js

--- contfracaneq2ns1.js
+++ contfracaneq2ns1.js
@@ -1,7 +1,8 @@
 // cont frac to series
-// current data for x/(1+x^2/(2+x^2/(3+x^2/(4+...))))
-// http://www.jstor.org/stable/2322267?Search=yes&resultItemClick=true&&searchUri=%2Faction%2FdoAdvancedSearch%3Fq4%3D%26amp%3Bisbn%3D%26amp%3Bf2%3Dall%26amp%3Bc6%3DAND%26amp%3Bf5%3Dall%26amp%3Bwc%3Don%26amp%3Bf1%3Dall%26amp%3Bq5%3D%26amp%3Bf6%3Dall%26amp%3Bq0%3Dcallan%26amp%3Bgroup%3Dnone%26amp%3Bsd%3D1988%26amp%3Bc4%3DAND%26amp%3Bq6%3D%26amp%3Bf0%3Dau%26amp%3Bpt%3DAmerican%2BMathematical%2BMonthly%26amp%3Bc2%3DAND%26amp%3Bc3%3DAND%26amp%3Bq3%3D%26amp%3Bf3%3Dall%26amp%3Bed%3D1988%26amp%3Bla%3D%26amp%3Bc5%3DAND%26amp%3Bq1%3D%26amp%3Bf4%3Dall%26amp%3Bacc%3Doff%26amp%3Bq2%3D%26amp%3Bc1%3DAND&loginSuccess=true&seq=1#page_scan_tab_contents
-// https://oeis.org/A039910/table
+// current data for x/(1+x^2/(3+x^2/(5+x^2/(7+...))))
+// https://en.wikipedia.org/wiki/Genocchi_number
+// https://oeis.org/A001469
+// https://books.google.ca/books?id=gE_MBQAAQBAJ&pg=PA29&lpg=PA29&dq=1,+1,+3,+17,+155,+2073,+38227,+929569&source=bl&ots=HL6FW2gueF&sig=gOcSkarW2A3hnW8HkcYvOCGpUf4&hl=en&sa=X&ei=3E2PVbzqBsu3yAS38KGQCg&ved=0CCsQ6AEwAw#v=onepage&q=1%2C%201%2C%203%2C%2017%2C%20155%2C%202073%2C%2038227%2C%20929569&f=false
 
 function s(n, m, r){
   if (n == "0")return "0";
@@ -13,14 +14,23 @@ function s(n, m, r){
                  s(sub(n, "1"), add(m, "1"), add(r, "1"))));
 }
 
-// normalize to v_n/(n!)^2*x^(2n+1)
+// normalize to 2^(2n-1)*v_n/(2n)!*x^(2n-1)
+
+function pow2n(n){
+  if (n == "0")return "1";
+  return mul("2", pow2n(sub(n, "1")));
+}
+
+pow2n = his(pow2n);
 
 function p(n){
   var f = s(n, "1", "2");
   var g = q(n);
   var r = redfrac(f, g);
-  //al(div(fact2(n), r[1]));
-  return mul(r[0], div(fact2(n), r[1]));
+  var h = mul(r[0], div(fact(mul("2", n)), r[1]));
+  //al(div(h, pow2n(add(mul("2", n), "1"))));
+  if (n == "0")return "0";
+  return "2^{" + sub(mul("2", n), "1") + "}\\cdot " + div(h, pow2n(sub(mul("2", n), "1")));
 }
 
 function q(n){
@@ -29,7 +39,7 @@ function q(n){
 }
 
 function q2(n){
-  return "\\left(" + n + "!\\right)^2";
+  return mul("2", n) + "!";
 }
 
 /*function p(n){
@@ -54,7 +64,7 @@ s = his3(s);
 q = his(q);
 
 function a(n){
-  return n;
+  return sub(mul("2", n), "1");
 }
 
 //a = his(a);
@@ -62,6 +72,10 @@ function a(n){
 function genc(n){
   return gen2(p, q2, "f", n, sgn, pow);
 }
+
+/*function genc(n){
+  return gen2red(p, q, "f", n, sgn, pow);
+}*/
 
 //out($.dsp($.map(g, arrof("0", "100"))));
 //genr("100")
